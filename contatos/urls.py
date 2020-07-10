3f7b57d5c2c86a9b174f60fcf24153e7fd641d6d
matheus <matheuscleber1998@gmail.com>
2020-07-09 23:43:34 -0300

    feature: upload static file

--- contatos/urls.py
+++ contatos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('store/<int:id>', views.store, name='store'),
-]
+    path('busca/', views.busca, name='busca'),
+] 
--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 # Create your views here.
 
@@ -18,4 +20,21 @@ def store(request, id):
   contato = Contato.objects.get(id=id)
   return render(request, 'pages/store.html',{
     'contato': contato
+  })
+
+def busca(request):
+  termo = request.GET.get('termo')
+  campos = Concat('nome', Value(' '), 'sobrenome')
+
+  contatos = Contato.objects.annotate(
+    nome_completo = campos
+  ).filter(
+    nome_completo__icontains = termo
+  )
+  
+  paginator = Paginator(contatos, 10)
+  page = request.GET.get('page')
+  contatos = paginator.get_page(page)
+  return render(request, 'pages/busca.html',{
+    'contatos': contatos
   })
