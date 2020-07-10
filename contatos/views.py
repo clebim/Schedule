4d6adeb910f1f306ab2b05cdfd01d033a4391c9c
matheus <matheuscleber1998@gmail.com>
2020-07-09 21:59:23 -0300

    feature: pagination

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Contato
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
   contatos = Contato.objects.all()
+  paginator = Paginator(contatos, 10)
+  page = request.GET.get('page')
+  contatos = paginator.get_page(page)
   return render(request, 'pages/index.html',{
     'contatos': contatos
   })
